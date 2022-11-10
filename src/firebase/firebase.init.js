c790a173c6a6a1587a0644c698eb15a6be3b60c8
Sadid <sfhsadid@gmail.com>
2022-11-10 21:24:21 +0600

    firebase init done

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDMJ8qcV-1SJaViiilSVJf5-HrUTCsyUw4",
-  authDomain: "service-review-client-da7a6.firebaseapp.com",
-  projectId: "service-review-client-da7a6",
-  storageBucket: "service-review-client-da7a6.appspot.com",
-  messagingSenderId: "590377061795",
-  appId: "1:590377061795:web:42e65cb2243f5898eb1e16",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
